e0118f4fb64c557e6058f6af30ced6525a671e6a
Bryan Wulfert <bwulfert@gmail.com>
2023-11-16 16:37:21 -0600

    completed react part1 exercises

--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,5 +1,8 @@
 import classes from './ChoresList.module.css';
 
 export default function ChoresList () {
-   return <p className={classes.choresText}>No content is here!</p>;
+   return  ( <div> <h3 className={classes.choresHeading}>Chores to do</h3>
+          <ul></ul><li className={classes.choresText}>Dishes</li>
+          <li className={classes.choresText}>Laundry</li> 
+          <li className={classes.choresText}>Cut the grass</li></div> )
 }
---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,10 @@
+
+export default function HobbyLinks() {
+let hobbyLinks = ["https://www.nhl.com/", "https://www.livestrong.com/cat/fitness/"]
+
+return (
+    <div>
+<a href={hobbyLinks[0]}>Link 1</a>
+<a href={hobbyLinks[1]}>Link 2</a> 
+</div> )
+}
