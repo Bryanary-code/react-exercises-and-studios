54d87c6e5c8f403cc23c4a2911c5e62ef3cc3ab1
Bryan Wulfert <bwulfert@gmail.com>
2023-11-16 13:34:36 -0600

    React exercies part1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Recently Released Titles";
+   let book1 = "https://www.booklistqueen.com/wp-content/uploads/the-mystery-guest-nita-prose.jpg";
+   let book2 = "https://www.booklistqueen.com/wp-content/uploads/the-frozen-river-ariel-lawhon.jpg";
+   let book3 = "https://www.booklistqueen.com/wp-content/uploads/the-future-naomi-alderman.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Mystery Guest by NITA PROSE" />
+         <img src={book2} alt="The Frozen River by ARIEL LAWHON" />
+         <img src={book3} alt="The Future by NAOMI ALDERMAN" />
       </div>      
    );
 }
