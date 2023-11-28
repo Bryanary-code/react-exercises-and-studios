d7d74bda9cd291083fcb4a67dde4f6214584f174
Bryan Wulfert <bwulfert@gmail.com>
2023-11-27 18:20:33 -0600

    complete react part2 studio

--- part2/studio/src/App.jsx
+++ part2/studio/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <>
       <RecipeImage />
       <RecipeName />
-      <RateARecipe rating={8} />
+      <RateARecipe rating={4} />
       <Button saveButton={true} />
       <AuthorInfo />
       <IngredientList />
